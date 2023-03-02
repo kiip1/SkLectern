0b84a74a581ea8432b5c806a6f81ce8587f4c668
kiip1 <25848425+kiip1@users.noreply.github.com>
2023-03-02 12:35:05 +0100

    Update tests

--- sklectern-bukkit/src/test/java/Test.java
+++ sklectern-bukkit/src/test/java/Test.java
@@ -1,11 +1,13 @@
 import be.seeseemelk.mockbukkit.MockBukkit;
 import nl.kiipdevelopment.sklectern.BukkitSkLectern;
-import nl.kiipdevelopment.sklectern.Script;
 import nl.kiipdevelopment.sklectern.SkLectern;
-import nl.kiipdevelopment.sklectern.lexer.ScriptLexer;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 final class Test {
     public static void main(String[] args) throws IOException, URISyntaxException {
@@ -19,18 +21,28 @@ final class Test {
 
         MockBukkit.load(BukkitSkLectern.class);
 
-        final Script script = new Script(SkLectern.instance()
-                .config()
-                .scriptFolder()
-                .resolve("capture.lsk"));
+//        final Script script = new Script(SkLectern.instance()
+//                .config()
+//                .scriptFolder()
+//                .resolve("capture.lsk"));
 
-        ScriptLexer.of(script.source())
-                .instance()
-                .iterator()
-                .forEachRemaining(System.out::println);
+//        ScriptLexer.of(script.source())
+//                .instance()
+//                .iterator()
+//                .forEachRemaining(System.out::println);
 
-        System.out.println(script.parse());
-        System.out.println(script.transform());
+//        System.out.println(script.parse());
+//        System.out.println(script.transform());
+
+        try (
+                Stream<Path> stream = Files.list(SkLectern.instance().config().distributionFolder());
+                BufferedWriter writer = Files.newBufferedWriter(Path.of("test-output.txt"))
+        ) {
+            for (Path path : stream.toList())
+                writer.write("Script " + path + ":\n" + Files.readString(path) + "\n");
+
+            writer.flush();
+        }
 
         MockBukkit.unmock();
     }
